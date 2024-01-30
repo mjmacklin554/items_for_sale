3bd40cc2187d1bb145ceaf9e45ae9ba5836de6f9
michael macklin <michael.macklin720@gmail.com>
2024-01-30 22:57:15 +1030

    Added a Search Function to the Items Project

--- items_db/urls.py
+++ items_db/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('list_item', views.list_item, name="list-item"),
     path('add_item', views.add_item, name="add-item"),
     path('show_list/<list_id>', views.show_list, name="show-list"),
+    path('search_item', views.search_item, name="search-item"),
    
 ]
